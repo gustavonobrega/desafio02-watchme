8f96e5d84cf4e1932a9aac62ad6cc67f015db5bb
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2021-03-15 20:48:00 -0300

    refactor code to Sidebar and Contento components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,36 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+import '../styles/content.scss';
+
+interface Movies {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: Movies[];
+  genreTitle: string;
+}
+
+export function Content({ movies, genreTitle }: ContentProps) {
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {genreTitle}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  );
 }
